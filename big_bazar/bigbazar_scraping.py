a41b93b7fc276fc3d5b14e995d8b752ccd234be2
da-faysal <faysal.e73@gmail.com>
2021-10-26 19:48:21 +0600

    Append to csv sequentially

--- big_bazar/bigbazar_scraping.py
+++ big_bazar/bigbazar_scraping.py
@@ -6,20 +6,41 @@ import numpy as np
 
 
 
-
 # Block browser notification
-chrome_options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications":2}
-chrome_options.add_experimental_option("prefs", prefs)
+options.add_experimental_option("prefs", prefs)
+
+
+# Suppress unnecessary warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 
 # Run in headless
-chrome_options.headless = True
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument('--no-sandbox')
+options.add_argument('--log-level=3')
 
 
 
-# Function to extract data
-def get_info(urls):
-    driver = webdriver.Chrome("/home/faysal/Desktop/big_bazar/chromedriver", options=chrome_options) # Set the path accordingly if windows machine
+# Create an empty csv with todays date
+TODAY = pd.to_datetime("today").strftime("%d_%b_%y")
+
+pd.DataFrame({
+    "product_name":np.nan,
+    "img_link":np.nan,
+    "offer_price":np.nan,
+    "original_price":np.nan,
+    "size":np.nan,
+    "cover_page":np.nan
+}, index=[0]).to_csv(f"{TODAY}_bigbazar_scraped_data.csv", index=None)
+
+
+
+
+def scrape_info(url):
     # Initialize empty list of variables to scrape
     product_name = []
     img_link = []
@@ -29,21 +50,26 @@ def get_info(urls):
     cover_page = []
     savings = []
     
-    for url in urls:
+    try:
+        driver = webdriver.Chrome("/home/faysal/Documents/utilities/chromedriver",
+                             options=options)
         driver.get(url)
-        time.sleep(5)
         
-        try:
-            driver.find_element_by_class_name("loginCross").click()
-        except:
-            pass
+        # time.sleep(5) 
+        # Will required without headless
         
+#         try:
+#             driver.find_element_by_class_name("loginCross").click()
+#         except:
+#             pass
+        # Will required without headless
+
         try:
             for _ in range(10):
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 time.sleep(4)
         except:
-            break
+            pass
 
         main_cont = driver.find_elements_by_class_name("col-xs-4.col-sm-4.similar")
         for cont in main_cont:
@@ -59,33 +85,33 @@ def get_info(urls):
                         product_name.append(f"{brand} {name}")
                     except:
                         product_name.append(np.nan)
-                    
+
                     try:
                         img_link.append(driver.find_element_by_class_name("slick-slide.slick-active.slick-current img").get_attribute("src"))
                     except:
                         img_link.append(np.nan)
-                        
+
                     try:
                         size.append(driver.find_element_by_css_selector(".details.quick-view-variant.skubg").text.strip())
                     except:
                         size.append(np.nan)
-                    
+
                     try:
                         offer_price.append(driver.find_element_by_class_name("quickView-memberPrice").text.strip())
                     except:
                         offer_price.append(np.nan)
-                    
+
                     try:
                         original_price.append(driver.find_element_by_class_name("quickView-discount").text.strip())
                     except:
                         original_price.append(np.nan)
-                    
+
                     cover_page.append(url)
-                    
+
                 driver.find_element_by_css_selector("span.QuikClose").click()
 
-                
-                
+
+
             except:
                 try:
                     product_name.append(cont.find_element_by_class_name("product-name").text.strip())
@@ -111,38 +137,59 @@ def get_info(urls):
                     original_price.append(cont.find_element_by_css_selector("div.list-price").text.strip())
                 except:
                     original_price.append(np.nan)
-                
+
                 cover_page.append(url)
-                    
-    # Create a df off scraped variables
-    df = pd.DataFrame({
+
+        driver.close()
+        df = pd.DataFrame({
         "product_name":product_name,
         "img_link":img_link,
         "offer_price":offer_price,
         "original_price":original_price,
         "size":size,
         "cover_page":cover_page
-    })
-    return df
+        })
+        with open(f"{TODAY}_bigbazar_scraped_data.csv", "a") as f:
+            df.to_csv(f, header=False, index=None)
+    except:
+        driver.close()
+        df = pd.DataFrame({
+                "product_name":np.nan,
+                "img_link":np.nan,
+                "offer_price":np.nan,
+                "original_price":np.nan,
+                "size":np.nan,
+                "cover_page":url}, index=[0])
+        
+        with open(f"{TODAY}_bigbazar_scraped_data.csv", "a") as f:
+            df.to_csv(f, header=False, index=None)
 
 
 
-# Read in csv data of category links
+
+# Read in csv data
 link_df = pd.read_csv("bigbazar_cat_link.csv")
 link_df["sub_cat1"]= np.where(link_df.sub_cat1.isna(),
          link_df.link.str.split("/").str[-1].str.split("-").str[:-1].str.join(" "),
          link_df.sub_cat1)
 
 
-# Save the the data
-TODAY = pd.to_datetime("today").strftime("%d_%b_%y")
-df = get_info(link_df.link) # Increase/decrease the category link as required
+
+# Scrape data
+for lnk in link_df.link.iloc[:3]:  # Increase or decrease the size of link. This is very time consuming
+    scrape_info(lnk)
+
+
+
+# Read in the file
+df = pd.read_csv(f"{TODAY}_bigbazar_scraped_data.csv")
+df.dropna(how="all")
 df.offer_price = df.offer_price.str.split("\n").str[0]
 m = pd.merge(df, link_df, left_on="cover_page", right_on="link")
 m["sub_cat2"] = m.link.str.split("/").str[-1].str.split("-").str[:-1].str.join(" ")
 m = m[["cover_page", "product_name", "size", "offer_price", "original_price",
        "img_link", "broad_cat", "sub_cat1", "sub_cat2"]]
 
-# Asave the output as csv
-m.to_csv(f"{TODAY}_bigbazar_data.csv", index=None)
+# Overwrite the current file
+m.to_csv(f"{TODAY}_bigbazar_scraped_data.csv", index=None)
 
